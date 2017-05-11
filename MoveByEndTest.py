cde728b0da86aacc65f5528a030ce5b1a49c9c42
Aldo Contreras <aldocontrego@gmail.com>
2017-05-11 23:34:07 +0200

    Update MoveByEndTest.py

--- MoveByEndTest.py
+++ MoveByEndTest.py
@@ -42,16 +42,16 @@ def moveByFunction():
 def moveByControl():
 	while movDone:
 	    drone.update()
-            try:
-                (dX, dY, dZ, dPsi, Event) = drone.moveByEnd
-                print "Drone moved [mts, rad]:", dX, dY, dZ, dPsi
-                Events = ["OK. Relative displacement done", "UNKNOWN", "BUSY", "NOTAVAILABLE", "INTERRUPTED"]
-                print "Move by event", Event, Events[Event]
-                if Event == 0 or Event == 5:    # Arrived or Interrupted
-                    movDone = False
-            except Exception, e:    # Catch error
-                print "Error getting data from drone, error:", e
-                pass
+        try:
+            (dX, dY, dZ, dPsi, Event) = drone.moveByEnd
+            print "Drone moved [mts, rad]:", dX, dY, dZ, dPsi
+            Events = ["OK. Relative displacement done", "UNKNOWN", "BUSY", "NOTAVAILABLE", "INTERRUPTED"]
+            print "Move by event", Event, Events[Event]
+            if Event == 0 or Event == 5:    # Arrived or Interrupted
+                movDone = False
+        except Exception, e:    # Catch error
+            print "Error getting data from drone, error:", e
+            pass
 	movDone = True
 	drone.wait( 4.0 )  # Waits () secs after arrive to its position
 

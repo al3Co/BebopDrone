13060e96b4fd6c4872ac725ba800fb9c056c8579
Aldo Contreras <aldocontreras@Aldos-MacBook-Air.local>
2017-06-05 13:24:41 +0200

    Takeoff time Sync

---
+++ 7_TakeoffSync.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+"""
+    Starting for usage at the computer
+    Do not delete the except Typer error. GPS data error, added when include GPS
+"""
+import time
+import math
+
+start_time = time.time()
+
+from core.bebop import *
+drone=Bebop()
+
+def main():
+    try:
+        drone.takeoff()
+        time.sleep(5)
+        drone.hover()
+        drone.land()
+        sys.exit(0)
+    except KeyboardInterrupt, e:
+        print
+        print "ManualControlException"
+        drone.hover()
+        if drone.flyingState is None or drone.flyingState == 1: # taking off
+            drone.emergency()
+        drone.land()
+        sys.exit(0)
+
+def fixedTime():
+    new_time = (((int(math.floor(start_time)) + 1) / 10) + 1) * 10
+    print("Wait %s seconds ..." % (new_time - start_time))
+    time.sleep(new_time - start_time)
+    main()
+
+if __name__ == "__main__":
+    fixedTime()
